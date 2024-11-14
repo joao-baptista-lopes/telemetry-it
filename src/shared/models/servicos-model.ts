2aa0f5bca342469228fa624f8abc2850dcfacc00
João Lopes <joao.lopes@minfin.gov.ao>
2024-11-14 15:51:30 +0100

    FEAT: mudança na interface de serviços

--- src/shared/models/servicos-model.ts
+++ src/shared/models/servicos-model.ts
@@ -4,10 +4,15 @@ export interface servicosModel{
     //Texto que vai para a secção de introdução no banner dos serviços
     introducaoSeccao: string,
     //Pontos característicos do serviço
-    caracteristicaServico:string[],
+    pontoFocal1: string,
+    pontoFocal2: string,
+    pontoFocal3: string,
+    pontoFocal4: string,
     // Descrição do serviço
     descricao: string, 
     // Lista de trabalhos realizados
-    trabalhosRealizados: string[]
+    trabalhosRealizados1: string,
+    trabalhosRealizados2: string,
+    trabalhosRealizados3: string
 
 }
